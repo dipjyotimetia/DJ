4e7755bf5af2960e8199820410b36fe779522674
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-02-25 22:58:24 +1100

    update dependencies

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 go build -v -o dist/ ./cmd/...
 
-gheye stubs --config stubs/config.yml
+dj stubs --config stubs/config.yml
 
-./dist/gheye stubs --config stubs/config.yml
+./dist/dj stubs --config stubs/config.yml
